1e452510b148567ecc4e7de152829cae38e6d225
shan <corallishan@gmail.com>
2013-10-10 20:19:53 -0400

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coral::Application.config.secret_token = '632e6ab0d42ead9a04506f53785cda1015a30f8a59d31385731cbbfcefc9f2fd677c6b0aaf875e77884e0e8d4c58852fedc0e7370770bcfb8e2b9c7aef28055a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Coral::Application.config.session_store :cookie_store, key: '_coral_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Coral::Application.config.session_store :active_record_store
